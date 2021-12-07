51933130ce4900cf4f035a3d04f187fc19c042ff
unknown <ubongabasi09@gmail.com>
2021-12-07 11:55:47 -0800

    Arguments

--- dictionaries.py
+++ dictionaries.py
@@ -1,4 +1,5 @@
 def user_profile(**details):
+    """display user's profile"""
     for k,v in details.items():
         print(f"{k}: {v}".title())
 user_profile(name="abasiubong",surname="effiong",occupation="lawyer",city="abuja")
